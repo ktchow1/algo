a12c582c0a63a581155f13dddc072948cf4174c2
ktchow1 <ktchow1@yahoo.com.hk>
2024-07-23 22:55:02 +0100

    update thread - sync atomic

--- 06_threading/test/test_thread_p.cpp
+++ 06_threading/test/test_thread_p.cpp
@@ -4,56 +4,45 @@
 #include<timer.h>
 #include<statistics.h>
 
-// ***************************** //
-// *** Posix thread affinity *** //
-// ***************************** //
-struct thread_data
+
+struct thread_result
 {
     std::uint32_t id;                         //  input
-    std::uint64_t aff_ns;                     // output (time for changing affinity) 
-    std::uint64_t pri_ns;                     // output (time for changing priority)
+    alg::statistics<std::uint64_t> aff_stat;  // output (time for changing affinity) 
+    alg::statistics<std::uint64_t> pri_stat;  // output (time for changing priority)
     std::unordered_set<std::uint32_t> cores0; // output (core used by this thread before setting affinity)
     std::unordered_set<std::uint32_t> cores1; // output (core used by this thread after setting affinity)
-
-    inline void reset(std::uint32_t n)
-    {
-        id = n;
-        aff_ns = 0;
-        pri_ns = 0;
-        cores0.clear();
-        cores1.clear();
-    }
 };
 
 void* thread_fct(void* arg)
 {
-    thread_data* data_ptr = reinterpret_cast<thread_data*>(arg);
+    thread_result* result_ptr = reinterpret_cast<thread_result*>(arg);
 
     // *** Sample core used for this thread *** //
-    for(std::uint32_t n=0; n!=20; ++n)
+    for(std::uint32_t n=0; n!=10; ++n)
     {
         std::this_thread::yield();
-        data_ptr->cores0.insert(sched_getcpu());
+        result_ptr->cores0.insert(sched_getcpu());
     }
 
     // *** Chanage affinity *** //
     alg::timer timer;
 
     timer.click();
-    alg::set_this_thread_affinity(data_ptr->id);
+    alg::set_this_thread_affinity(result_ptr->id);
     timer.click();
-    data_ptr->aff_ns = timer.time_elapsed_in_nsec();
+    result_ptr->aff_stat.add(timer.time_elapsed_in_nsec());
 
     timer.click();
     alg::set_this_thread_policy(SCHED_RR);
     timer.click();
-    data_ptr->pri_ns = timer.time_elapsed_in_nsec();
+    result_ptr->pri_stat.add(timer.time_elapsed_in_nsec());
 
     // *** Sample core used for this thread *** //
-    for(std::uint32_t n=0; n!=20; ++n)
+    for(std::uint32_t n=0; n!=10; ++n)
     {
         std::this_thread::yield();
-        data_ptr->cores1.insert(sched_getcpu());
+        result_ptr->cores1.insert(sched_getcpu());
     }
     return NULL;
 }
@@ -61,23 +50,22 @@ void* thread_fct(void* arg)
 void test_pthread()
 {
     constexpr std::uint32_t num_threads = 3;
-    alg::statistics<std::uint64_t> aff_stat;               // time for changing affinity 
-    alg::statistics<std::uint64_t> pri_stat;               // time for changing priority      
-    std::unordered_set<std::uint32_t> cores0[num_threads]; // core-id before setting affinity
-    std::unordered_set<std::uint32_t> cores1[num_threads]; // core-id after  setting affinity
+    thread_result results[num_threads];
+    for (std::uint32_t n=0; n!=num_threads; ++n)
+    {
+        results[n].id = n;
+    }
 
     std::cout << "\nNext test progress ";
     for(std::uint32_t n=0; n!=1000; ++n)
     {
-        if(n%20==0) std::cout << "." << std::flush;        
+        if (n%20==0) std::cout << "." << std::flush;        
         pthread_t threads[num_threads];
-        thread_data data[num_threads];
 
         // *** Create *** //
         for(std::uint32_t n=0; n!=num_threads; ++n) 
         {
-            data[n].reset(n);
-            pthread_create(threads+n, NULL, thread_fct, data+n);
+            pthread_create(threads+n, NULL, thread_fct, results+n);
             // arg[1] = pthread_attr_t which is NULL for default
             // arg[2] = function taking void* returning void*
             // arg[3] = function argument
@@ -88,24 +76,15 @@ void test_pthread()
         {
             pthread_join(threads[n], NULL);        
         }
-
-        // *** Measurement *** //
-        for(std::uint32_t n=0; n!=num_threads; ++n)            
-        {
-            aff_stat.add(data[n].aff_ns);
-            pri_stat.add(data[n].pri_ns);
-            cores0[n].insert(data[n].cores0.begin(), data[n].cores0.end());
-            cores1[n].insert(data[n].cores1.begin(), data[n].cores1.end());
-        }
     }
 
     std::cout << "\n\n[Core used by different thread]";
     for(std::uint32_t n=0; n!=num_threads; ++n)            
     {
-        std::cout << "\nthread " << n << " before assigning affinity : "; for(const auto&x : cores0[n]) std::cout << x << " ";
-        std::cout << "\nthread " << n << " after  assigning affinity : "; for(const auto&x : cores1[n]) std::cout << x << " ";
+        std::cout << "\nthread " << n << " before assigning affinity : "; for(const auto&x : results[n].cores0) std::cout << x << " ";
+        std::cout << "\nthread " << n << " after  assigning affinity : "; for(const auto&x : results[n].cores1) std::cout << x << " ";
     }
-    std::cout << "\n\n[Time for setting affinity]" << aff_stat.get_string();
-    std::cout << "\n\n[Time for setting priority]" << pri_stat.get_string() << "\n";
+    std::cout << "\n\n[Time for setting affinity]" << results[0].aff_stat.get_string();
+    std::cout << "\n\n[Time for setting priority]" << results[0].pri_stat.get_string() << "\n";
 }
 
