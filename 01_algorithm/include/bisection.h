8f9a8d26476f36645f349f5d50bdbf0496269705
ktchow1 <ktchow1@yahoo.com.hk>
2024-12-21 02:07:09 +0000

    update algo

--- 01_algorithm/include/bisection.h
+++ 01_algorithm/include/bisection.h
@@ -3,59 +3,73 @@
 #include<functional>
 #include<optional>
 
-/*
 namespace alg
 {
-    inline std::optional<double> bisection(std::function<double(double)>& fct, double x0, double x1)
+    std::optional<double> bisection(std::function<double(double)>& fct, double x0, double x1)
     {
-        // 1. check edge case, or init y
+        // 1. check edge case (or init y in here)
         double y0 = fct(x0);
         double y1 = fct(x1);
 
         // 2. check solution existence
-        if (x0 * x1 > 0) return std::nullopt;
+        if (y0 * y1 > 0) return std::nullopt;
 
         // 3. check stop condition
+        while(fabs(x0 - x1) < 0.0001)
+        {
+            // 4. mid point
+            double xm = (x0 + x1) / 2;
+            double ym = fct(xm);
+            
+            // 5. bisection
+            if (y0 * ym > 0)
+            {
+                x0 = xm;
+                y0 = ym;
+            }
+            else 
+            {
+                x1 = xm;
+                y1 = ym;
+            }
+        }            
 
-
-        // 4. mid point
-        
-        // 5. bisection
-        
         // 6. answer
-
-
-
+        return std::make_option((x0 + x1) / 2);
     }
 
-    inline double bisection(const std::function<double(double)>& fct, double x0, double x1)
+    double bisection(const std::function<double(double)>& fct, double x0, double x1)
     {
+        return 0;
     }
 }
 
 namespace alg
 {
-    inline std::optional<std::size_t> bisection(const std::vector<std::int64_t>& vec, std::int64_t target)
+    std::optional<std::size_t> bisection(const std::vector<std::int32_t>& vec, std::int32_t target)
     {
+        return std::nullopt;
     }
 
-    inline std::optional<std::size_t> bisection_impl(const std::vector<std::int64_t>& vec, std::int64_t target, std::size_t x0, std::size_t x1)
+    std::optional<std::size_t> bisection_impl(const std::vector<std::int32_t>& vec, std::int32_t target, std::size_t x0, std::size_t x1)
     {
-    }
-}
-
-namespace alg
-{
-    inline std::optional<std::size_t> mode_bisection(const std::vector<std::int64_t>& vec)
-    {
+        return std::nullopt;
     }
 }
 
 namespace alg
 {
-    inline std::optional<std::size_t> rotated_bisection(const std::vector<std::int64_t>& vec, std::int64_t target)
+    std::optional<std::size_t> mode_bisection(const std::vector<std::int32_t>& vec)
     {
+        return std::nullopt;
     }
 }
 
-*/
+namespace alg
+{
+    std::optional<std::size_t> rotated_bisection(const std::vector<std::int32_t>& vec, std::int32_t target)
+    {
+        return std::nullopt;
+    }
+}
+
