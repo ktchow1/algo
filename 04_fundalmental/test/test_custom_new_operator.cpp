7c6c601e2fee04d4f7ff9309d3390a24dc0d6681
ktchow1 <ktchow1@yahoo.com.hk>
2025-01-27 22:24:23 +0000

    update algo#

--- 04_fundalmental/test/test_custom_new_operator.cpp
+++ 04_fundalmental/test/test_custom_new_operator.cpp
@@ -6,6 +6,13 @@
 #include<utility.h>
 
 
+// *********************************************************************** //
+// Custom new and delete 
+//
+//
+//
+//
+// *********************************************************************** //
 namespace alg
 {
     std::vector<std::string> new_operator_invoked;
@@ -15,36 +22,38 @@ namespace alg
     {
         A(std::uint32_t x, std::uint32_t y, std::uint32_t z) : m_x(x), m_y(y), m_z(z)
         {
-            std::cout << "\nconstructor invoked";
+            new_operator_invoked.push_back("constructor invoked");
         }
 
         ~A()
         {
-            std::cout << "\ndestructor invoked";
+            new_operator_invoked.push_back("destructor invoked");
         }
 
         void* operator new (size_t n) 
         { 
-            std::cout << "\nnew operator invoked, size=" << n << std::flush; 
+            std::stringstream ss;
+            ss << "new operator invoked, " << n << " bytes"; 
+            new_operator_invoked.push_back(ss.str());
             return buffer;
         }
 
         void* operator new[] (size_t n) 
         { 
-            std::cout << "\nnew[] operator invoked, size=" << n << std::flush; 
+            std::stringstream ss;
+            ss << "new[] operator invoked, " << n << " bytes"; 
+            new_operator_invoked.push_back(ss.str());
             return buffer;
         }
 
         void operator delete (void* ptr) 
         {
-            std::cout << "\ndelete operator invoked" << std::flush; 
-            ptr = nullptr;
+            new_operator_invoked.push_back("delete operator invoked");
         }
 
         void operator delete[] (void* ptr) 
         {
-            std::cout << "\ndelete[] operator invoked" << std::flush; 
-            ptr = nullptr;
+            new_operator_invoked.push_back("delete[] operator invoked");
         }
 
         std::uint32_t m_x;
@@ -53,26 +62,90 @@ namespace alg
     };
 }
 
+
 void test_custom_new_operator()
 {
+    // *************** //
+    // *** Pointer *** //
+    // *************** //
     auto* p0 = new alg::A{11,12,13};
     assert((std::uint64_t)p0 == (std::uint64_t)alg::buffer);
     delete p0;
 
-    auto* p1 = new alg::A[1]{{11,12,13}};
+    assert("new operator invoked, 12 bytes"); // <--- new operator is invoked before constructor
+    assert("constructor invoked");
+    assert("destructor invoked");
+    assert("delete operator invoked");        // <--- delete operator is invoked after destructor
+    alg::new_operator_invoked.clear();
+
+
+    // ******************** //
+    // *** Array size 1 *** //
+    // ******************** //
+    auto* p1 = new alg::A[1]
+    {
+        {11,12,13}
+    };
+    auto* p1_offset = reinterpret_cast<char*>(p1) - sizeof(std::uint64_t);
     assert((std::uint64_t)p1 == (std::uint64_t)alg::buffer + sizeof(std::uint64_t));
+    assert(*(reinterpret_cast<std::uint64_t*>(alg::buffer)) == 1); 
+    assert(*(reinterpret_cast<std::uint64_t*>(p1_offset))   == 1);  
     delete[] p1;
 
-    auto* p2 = new alg::A[4]{{11,12,13},{21,22,23},{31,32,33},{41,42,43}};
+    assert("new[] operator invoked, 20 bytes");
+    assert("constructor invoked");
+    assert("destructor invoked");
+    assert("delete[] operator invoked");
+    alg:: new_operator_invoked.clear();
+
+
+    // ******************** //
+    // *** Array size 4 *** //
+    // ******************** //
+    auto* p2 = new alg::A[4]
+    {
+        {11,12,13},
+        {21,22,23},
+        {31,32,33},
+        {41,42,43}
+    };
+    auto* p2_offset = reinterpret_cast<char*>(p2) - sizeof(std::uint64_t);
     assert((std::uint64_t)p2       == (std::uint64_t)alg::buffer + sizeof(std::uint64_t));
     assert((std::uint64_t)(&p2[0]) == (std::uint64_t)alg::buffer + sizeof(std::uint64_t) + sizeof(alg::A) * 0);
     assert((std::uint64_t)(&p2[1]) == (std::uint64_t)alg::buffer + sizeof(std::uint64_t) + sizeof(alg::A) * 1);
     assert((std::uint64_t)(&p2[2]) == (std::uint64_t)alg::buffer + sizeof(std::uint64_t) + sizeof(alg::A) * 2);
     assert((std::uint64_t)(&p2[3]) == (std::uint64_t)alg::buffer + sizeof(std::uint64_t) + sizeof(alg::A) * 3);
-    std::cout << "\nsize = " << *(reinterpret_cast<std::uint64_t*>(alg::buffer));
+    assert(*(reinterpret_cast<std::uint64_t*>(alg::buffer)) == 4); 
+    assert(*(reinterpret_cast<std::uint64_t*>(p1_offset))   == 4); 
     delete[] p2;
 
-    auto* p3 = new alg::A[7]{{11,12,13},{21,22,23},{31,32,33},{41,42,43},{51,52,53},{61,62,63},{71,72,73}};
+    assert("new[] operator invoked, 56 bytes");
+    assert("constructor invoked");
+    assert("constructor invoked");
+    assert("constructor invoked");
+    assert("constructor invoked");
+    assert("destructor invoked");
+    assert("destructor invoked");
+    assert("destructor invoked");
+    assert("destructor invoked");
+    assert("delete[] operator invoked");
+    alg:: new_operator_invoked.clear();
+
+
+    // ******************** //
+    // *** Array size 7 *** //
+    // ******************** //
+    auto* p3 = new alg::A[7]
+    {
+        {11,12,13},
+        {21,22,23},
+        {31,32,33},
+        {41,42,43},
+        {51,52,53},
+        {61,62,63},
+        {71,72,73}
+    };
+    auto* p3_offset = reinterpret_cast<char*>(p3) - sizeof(std::uint64_t);
     assert((std::uint64_t)p3       == (std::uint64_t)alg::buffer + sizeof(std::uint64_t));
     assert((std::uint64_t)(&p3[0]) == (std::uint64_t)alg::buffer + sizeof(std::uint64_t) + sizeof(alg::A) * 0);
     assert((std::uint64_t)(&p3[1]) == (std::uint64_t)alg::buffer + sizeof(std::uint64_t) + sizeof(alg::A) * 1);
@@ -81,8 +154,30 @@ void test_custom_new_operator()
     assert((std::uint64_t)(&p3[4]) == (std::uint64_t)alg::buffer + sizeof(std::uint64_t) + sizeof(alg::A) * 4);
     assert((std::uint64_t)(&p3[5]) == (std::uint64_t)alg::buffer + sizeof(std::uint64_t) + sizeof(alg::A) * 5);
     assert((std::uint64_t)(&p3[6]) == (std::uint64_t)alg::buffer + sizeof(std::uint64_t) + sizeof(alg::A) * 6);
-    std::cout << "\nsize = " << *(reinterpret_cast<std::uint64_t*>(alg::buffer));
+    assert(*(reinterpret_cast<std::uint64_t*>(alg::buffer)) == 7); 
+    assert(*(reinterpret_cast<std::uint64_t*>(p1_offset))   == 7); 
     delete[] p3;
+
+    assert("new[] operator invoked, 92 bytes");
+    assert("constructor invoked");
+    assert("constructor invoked");
+    assert("constructor invoked");
+    assert("constructor invoked");
+    assert("constructor invoked");
+    assert("constructor invoked");
+    assert("constructor invoked");
+    assert("destructor invoked");
+    assert("destructor invoked");
+    assert("destructor invoked");
+    assert("destructor invoked");
+    assert("destructor invoked");
+    assert("destructor invoked");
+    assert("destructor invoked");
+    assert("delete[] operator invoked");
+    alg:: new_operator_invoked.clear();
+
+
+    print_summary("custom new and delete operator", "succeeded");
 }    
 
 
