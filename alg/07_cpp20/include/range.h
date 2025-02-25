81d06810e64780086ed1d455da7cc0f919eff6c0
ktchow1 <ktchow1@yahoo.com.hk>
2025-02-25 22:52:33 +0000

    update algo - logic change in range lib

--- alg/07_cpp20/include/range.h
+++ alg/07_cpp20/include/range.h
@@ -1,6 +1,7 @@
 #pragma once
 #include<cstdint>
 #include<functional>
+#include<variant>
 
 
 namespace alg
@@ -26,66 +27,76 @@ namespace alg
     };
 
 
-    // ********************************************** //
-    // *** A container view, with lazy evaluation *** //
-    // ********************************************** //
+    // ********************** //
+    // *** Container view *** //
+    // ********************** //
     template<typename C> // C = container
     struct lazy_view
     {
     public:
-        using T = typename C::value_type;
+        using              T = typename C::value_type;
+        using    filter_type = std::function<bool(T)>;
+        using transform_type = std::function<T(T)>;
 
-        using  filt_type = std::function<bool(T)>;
-        using  tran_type = std::function<T(T)>;
         struct take_type
         {
             std::uint32_t m_count;
             std::uint32_t m_limit;
-        };
+        }; 
+
+        using logic = std::variant<filter_type, transform_type, take_type>;
+
 
     public:
-        lazy_view(C& container) : i0(container.begin()), 
-                                  i1(container.end()),
-                                  m_filter{},
-                                  m_transform{},
-                                  m_take{}
+        lazy_view(C& container) : m_begin(container.begin()), m_end(container.end()), m_logics{}
         {
         }
 
-        // casting operator
+        // *********************************************** //
+        // *** Casting operator - with lazy evaluation *** //
+        // *********************************************** //
         operator C()
         {
-            // This is lazy evaluation.
             C output;
-            for(auto i=i0; i!=i1; ++i)
+            for(auto iter=m_begin; iter!=m_end; ++iter)
             {
-                if (m_filter(*i))
-                {
-                    output.push_back(m_transform(*i));
-
-                    ++m_take.m_count;
-                    if (m_take.m_count == m_take.m_limit) return output;
+                auto x = *iter;
+                for(auto& logic:m_logics)
+                { 
+                    if (logic.index() == 0)
+                    {
+                        if (!std::get<0>(logic)(x)) break; // break inner-for, but continue outer-for
+                    }
+                    else if (logic.index() == 1)
+                    {
+                        x = std::get<1>(logic)(x);
+                    }
+                    else if (logic.index() == 2)
+                    {
+                        if (std::get<2>(logic).m_count == 
+                            std::get<2>(logic).m_limit) return output;
+                        ++  std::get<2>(logic).m_count;
+                    }
+                    output.push_back(x);
                 }
-            }
+            } 
             return output;
         }
 
         auto begin() const // Todo : not completed
         {
-            return i0;
+            return m_begin;
         }
 
         auto end() const 
         {
-            return i1;
+            return m_end;
         }
 
     public:
-        typename C::iterator i0;
-        typename C::iterator i1;    
-        filt_type m_filter;
-        tran_type m_transform;
-        take_type m_take;
+        typename C::iterator m_begin;
+        typename C::iterator m_end;    
+        std::vector<logic>   m_logics;
     };
 
 
@@ -93,10 +104,10 @@ namespace alg
     // *** The pipeline expression (in test) resolves into the following operators, returning lazy_view. *** //
     // ***************************************************************************************************** //
     template<typename C, typename FCT>
-    lazy_view<C> operator | (C& lhs, filter<FCT> rhs)
+    lazy_view<C> operator | (C& lhs, const filter<FCT>& rhs)
     {
         lazy_view<C> output(lhs);
-        output.m_filter = rhs.value; 
+        output.m_logics.push_back(typename lazy_view<C>::filter_type{rhs.value}); 
         return output;
     }
 
@@ -108,24 +119,23 @@ namespace alg
     // *** Cascading *** //
     // ***************** //
     template<typename C, typename FCT>
-    lazy_view<C> operator | (lazy_view<C> lhs, filter<FCT> rhs)
+    lazy_view<C> operator | (lazy_view<C>&& lhs, const filter<FCT>& rhs) // Is "lhs" universal reference or rvalue reference?
     {
-        lhs.m_filter = rhs.value; 
+        lhs.m_logics.push_back(typename lazy_view<C>::filter_type{rhs.value}); 
         return lhs;
     }
 
     template<typename C, typename FCT>
-    lazy_view<C> operator | (lazy_view<C> lhs, transform<FCT> rhs)
+    lazy_view<C> operator | (lazy_view<C>&& lhs, const transform<FCT>& rhs)
     {
-        lhs.m_transform = rhs.value; 
+        lhs.m_logics.push_back(typename lazy_view<C>::transform_type{rhs.value}); 
         return lhs;
     }
 
     template<typename C>
-    lazy_view<C> operator | (lazy_view<C> lhs, take rhs)
+    lazy_view<C> operator | (lazy_view<C>&& lhs, const take& rhs)
     {
-        lhs.m_take.m_count = 0;
-        lhs.m_take.m_limit = rhs.value; 
+        lhs.m_logics.push_back(typename lazy_view<C>::take_type{0, rhs.value});
         return lhs;
     }
 }
