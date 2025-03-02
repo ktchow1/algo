adc52ebff07822a28c1736e24e4906e3b1e43ee5
ktchow1 <ktchow1@yahoo.com.hk>
2025-03-02 18:16:10 +0000

    update algo

--- alg/07_cpp20/test/test_concepts_apply.cpp
+++ alg/07_cpp20/test/test_concepts_apply.cpp
@@ -1,7 +1,7 @@
 #include<iostream>
 #include<cassert>
 #include<cstdint>
-#include<vector>
+#include<string>
 #include<concepts>
 
 // *** alg *** //
@@ -13,24 +13,164 @@
 // ***************************** //
 // *** Applying std concepts *** //
 // ***************************** //
+// There are 3 ways to apply concept :
+// 1. as requires-clause after template parameter   (supports conjunction / disjunction of multiple concepts)
+// 2. as requires-clause after function prototype   (supports conjunction / disjunction of multiple concepts)
+// 3. as requires-clause in template parameter list (does not support conjunction / disjunction) <--- 1st template parameter is the one on which constraint applied
+// 4. as requires-requires pattern, which is a requires-clause plus a requires-expression
+//
+// 
+// requires-clause      : apply concept to template class or template function
+// requires-expresssion : define a new concept
+//
+//
+// Format of a requires-clause : 
+// requires std::xxx<T>          <--- where xxx is a concept
+// requires std::xxx<T,U,...>    <--- where xxx is a concept
+//    
 namespace test
 {
-    // define is_traits
-    // compare with SFINAE
+    // ********************************************** //
+    // *** std concepts with 1 template parameter *** //
+    // ********************************************** //
+    struct type_A { type_A() = delete;     type_A(std::uint32_t){}                                         };
+    struct type_B { type_B(){}             type_B(const type_B&) = delete;     type_B(type_B&&){}          };
+    struct type_C { type_C(){}             type_C(const type_C&){}             type_C(type_C&&) = delete;  };
+
+    template<typename T, typename U> 
+    void tester_no_constraint(const T&, const U&) {}
+
+    template<typename T, typename U> requires std::default_initializable<T>
+    void tester_no_A_method1(const T&, const U&) {}
+    template<typename T, typename U> requires std::copy_constructible<T>
+    void tester_no_B_method1(const T&, const U&) {}
+    template<typename T, typename U> requires std::move_constructible<T>
+    void tester_no_C_method1(const T&, const U&) {}
+
+    template<typename T, typename U> 
+    void tester_no_A_method2(const T&, const U&) requires std::default_initializable<T> {}
+    template<typename T, typename U> 
+    void tester_no_B_method2(const T&, const U&) requires std::copy_constructible<T> {}
+    template<typename T, typename U> 
+    void tester_no_C_method2(const T&, const U&) requires std::move_constructible<T> {}
+
+    template<std::default_initializable T, typename U> 
+    void tester_no_A_method3(const T&, const U&) {}
+    template<std::copy_constructible T, typename U> 
+    void tester_no_B_method3(const T&, const U&) {}
+    template<std::move_constructible T, typename U> 
+    void tester_no_C_method3(const T&, const U&) {}
 
 
+    // *********************************************** //
+    // *** std concepts with 2 template parameters *** //
+    // *********************************************** //
+    // The order of template parameter in std::is_base_of<B,D> is different from that in std::derived_from<D,B>
+      
+    struct type_D : public type_B {};
+
+    template<typename T, typename U> requires std::derived_from<T, type_B>
+    void tester_derived_from_B_method1(const T&, const U&) {}
+    template<typename T, typename U> requires std::derived_from<T, U>
+    void tester_derived_from_U_method1(const T&, const U&) {}
+
+
+
+
+
+
+    // *********************************************** //
+    // *** std concepts with 2 template parameters *** //
+    // *********************************************** //
+    template<typename T, typename U, typename V>
+    void tester_no_constraint(const T&, const U&, const V&) {}
+
+    // ************************************* //
+    // *** simple definition of concepts *** //
+    // ************************************* //
+
+
+    // ************************* //
+    // *** requires-requires *** //
+    // ************************* //
 
 }
 
 
+void test_concepts_apply_constraint_on_1_para()
+{
+    // Comment lines cannot compile due to concept
+    test::tester_no_constraint(test::type_A{123}, std::string{"abc"});
+    test::tester_no_constraint(test::type_B{},    std::string{"abc"});
+    test::tester_no_constraint(test::type_C{},    std::string{"abc"});
+
+//  test::tester_no_A_method1(test::type_A{123}, std::string{"abc"});
+    test::tester_no_A_method1(test::type_B{},    std::string{"abc"});
+    test::tester_no_A_method1(test::type_C{},    std::string{"abc"});
+    test::tester_no_B_method1(test::type_A{123}, std::string{"abc"});
+//  test::tester_no_B_method1(test::type_B{},    std::string{"abc"});
+//  test::tester_no_B_method1(test::type_C{},    std::string{"abc"}); // std::copy_constructible = requires copy_constructible AND move_constructible
+    test::tester_no_C_method1(test::type_A{123}, std::string{"abc"});
+    test::tester_no_C_method1(test::type_B{},    std::string{"abc"}); // std::move_constructible = requires move_constructible only
+//  test::tester_no_C_method1(test::type_C{},    std::string{"abc"});
+
+//  test::tester_no_A_method2(test::type_A{123}, std::string{"abc"});
+    test::tester_no_A_method2(test::type_B{},    std::string{"abc"});
+    test::tester_no_A_method2(test::type_C{},    std::string{"abc"});
+    test::tester_no_B_method2(test::type_A{123}, std::string{"abc"});
+//  test::tester_no_B_method2(test::type_B{},    std::string{"abc"});
+//  test::tester_no_B_method2(test::type_C{},    std::string{"abc"}); // std::copy_constructible = requires copy_constructible AND move_constructible
+    test::tester_no_C_method2(test::type_A{123}, std::string{"abc"});
+    test::tester_no_C_method2(test::type_B{},    std::string{"abc"}); // std::move_constructible = requires move_constructible only
+//  test::tester_no_C_method2(test::type_C{},    std::string{"abc"});
+  
+//  test::tester_no_A_method3(test::type_A{123}, std::string{"abc"});
+    test::tester_no_A_method3(test::type_B{},    std::string{"abc"});
+    test::tester_no_A_method3(test::type_C{},    std::string{"abc"});
+    test::tester_no_B_method3(test::type_A{123}, std::string{"abc"});
+//  test::tester_no_B_method3(test::type_B{},    std::string{"abc"});
+//  test::tester_no_B_method3(test::type_C{},    std::string{"abc"}); // std::copy_constructible = requires copy_constructible AND move_constructible
+    test::tester_no_C_method3(test::type_A{123}, std::string{"abc"});
+    test::tester_no_C_method3(test::type_B{},    std::string{"abc"}); // std::move_constructible = requires move_constructible only
+//  test::tester_no_C_method3(test::type_C{},    std::string{"abc"});
+  
+    print_summary("coocepts - apply constraint on 1 para", "succeeded");
+}
+
+
+void test_concepts_apply_constraint_on_2_para()
+{
+
+    print_summary("coocepts - apply constraint on 2 para", "succeeded");
+}
+
+
+void test_concepts_apply_constraint_on_3_para()
+{
+    print_summary("coocepts - apply constraint on 3 para", "succeeded");
+}
+
+
+void test_concepts_requires_requires()
+{
+    print_summary("coocepts - requires requires", "succeeded");
+}
+
+
+void test_concepts_with_abbv_function_template()
+{
+    print_summary("coocepts - with abbv function template", "succeeded");
+}
 
 
 void test_concepts_apply()
 {
-
+    test_concepts_apply_constraint_on_1_para();
+    test_concepts_apply_constraint_on_2_para();
+    test_concepts_apply_constraint_on_3_para();
+    test_concepts_requires_requires();
+    test_concepts_with_abbv_function_template();
 }
-
-
 
 
 
--- alg/07_cpp20/test/test_concepts_define.cpp
+++ alg/07_cpp20/test/test_concepts_define.cpp
@@ -13,14 +13,27 @@
 // ***************************** //
 // *** Defining alg concepts *** //
 // ***************************** //
+// Concept is related to traits. The former is usually built on top of the latter.
+//
+// Naming in STL : 
+// * std::is_xxx_able  <T> = traits
+// * std::is_xxx_able_v<T> = std::is_xxx_able<T>::value
+// * std::is_xxx_able_t<T> = std::is_xxx_able<T>::type
+// * std::   xxx_able  <T> = concept based on std::is_xxx_able<T>
+//
+//
+//
+//
 namespace alg
 {
-    struct typeA {};
+/*  struct typeA {};
     struct typeB {};
     struct typeC {};
     struct typeD {};
     struct typeE {};
-
+*/
+    // define is_traits
+    // compare with SFINAE
 
 }
 
